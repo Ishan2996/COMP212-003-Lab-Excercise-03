38a4b9bebed47833405cfd45e4e6dae5bad7404f
Ishan S Sharma <Ishan S Sharma@DESKTOP-C6LS930>
2017-01-23 12:57:35 -0500

    MenuTest

---
+++ MenuTest/MenuTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MenuTest
+{
+    public partial class MenuTest : Form
+    {
+        public MenuTest()
+        {
+            InitializeComponent();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This is about Menu", "Menu Demo", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void MenuTest_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
